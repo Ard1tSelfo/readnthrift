03e8535860cd402fcb3c47f03bc465c270b6fcc0
ard1tselfo <ardit.selfo123@hotmail.com>
2020-07-07 11:26:58 +0200

    implemented backend endpoint for getting book by id

--- backend/src/controllers/book.js
+++ backend/src/controllers/book.js
@@ -12,23 +12,21 @@ const booklist = async (req, res) => {
     }
 };
 
-const createbook = async (req, res) => {
-    if (Object.keys(req.body).length === 0) return res.status(400).json({
-        error: 'Bad Request',
-        message: 'The request body is empty'
-    });
+const getBookById = async (req, res) => {
+
+    let id = req.params.id
     try {
-      let book = await BookModel.create(req.body);
-      return res.status(201).json(book)
-    } catch(err) {
-      return res.status(500).json({
-        error: 'Internal server error',
-        message: err.message
-      });
+        let book = await BookModel.findOne({ _id: id}).exec();
+        return res.status(200).json(book);
+    } catch (err) {
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        });
     }
-};
+}
 
 module.exports = {
     booklist,
-    createbook
+    getBookById
 }
--- backend/src/routers/book.js
+++ backend/src/routers/book.js
@@ -1,8 +1,9 @@
 const express  = require('express');
 const router   = express.Router();
 const BookController = require('../controllers/book');
+const auth = require('../middleware/auth');
 
 router.get('/books', BookController.booklist); // List of all books
-router.post('/newbook', BookController.createbook); // Add new book
+router.get('/books/:id', BookController.getBookById); 
 
 module.exports = router;
