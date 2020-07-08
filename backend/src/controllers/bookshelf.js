f88f3d7def5211f04c3a79212f6e863fede8c46d
ard1tselfo <ardit.selfo123@hotmail.com>
2020-07-08 19:38:14 +0200

    added get method for bookshelves of a specific user

--- backend/src/controllers/bookshelf.js
+++ backend/src/controllers/bookshelf.js
@@ -3,8 +3,8 @@ const UserModel = require("../models/User");
 
 const bookshelflist = async (req, res) => {
     try {
-        let bookshelfs = await BookshelfModel.find({}).exec();
-        return res.status(200).json(bookshelfs);
+        let bookshelves = await BookshelfModel.find({"user" : req.body.user}).exec();
+        return res.status(200).json(bookshelves);
     } catch (err) {
         return res.status(500).json({
             error: "Internal server error",
@@ -19,9 +19,10 @@ const createbookshelf = async (req, res) => {
             error: "Bad Request",
             message: "The request body is empty",
         });
+
     try {
-        let bookshelf = await BookshelfModel.create(req.body);
-        let user = await UserModel.findById(req.body.user);
+        const bookshelf = await BookshelfModel.create(req.body);
+        const user = await UserModel.findById(req.body.user);
         user.bookshelves = [...user.bookshelves, bookshelf];
         user.save();
         return res.status(201).json(bookshelf);
@@ -40,13 +41,13 @@ const updatebookshelf = async (req, res) => {
             message: "The request body is empty",
         });
     try {
-        let bookshelf = await BookshelfModel.findByIdAndUpdate(
+        // check if book is there, provide feedback
+        const bookshelf = await BookshelfModel.findByIdAndUpdate(
             req.params.id,
             { $push: req.body },
             {
                 safe: true,
                 new: true,
-                runValidators: true,
             }
         ).exec();
         return res.status(201).json(bookshelf);
--- backend/src/routers/bookshelf.js
+++ backend/src/routers/bookshelf.js
@@ -3,7 +3,8 @@ const router   = express.Router();
 const BookshelfController = require('../controllers/bookshelf');
 const auth = require('../middleware/auth');
 
-router.post('/users/me/bookshelves', BookshelfController.createbookshelf); 
+router.post('/users/me/bookshelves/new', BookshelfController.createbookshelf); 
 router.put('/users/me/bookshelves/:id', BookshelfController.updatebookshelf); 
+router.get('/users/me/bookshelves', BookshelfController.bookshelflist);
 
 module.exports = router;
