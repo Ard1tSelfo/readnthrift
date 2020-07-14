eb5b3aad1d13935456cf61c7de423153012b83e0
Alla Sergeeva <alla.sergeeva@tum.de>
2020-07-14 18:08:53 +0200

    added offerService (needs refactoring)

---
+++ frontend/src/services/OfferService.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+// TODO: make axios global to set this for each request - even in other services
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default class OfferService {
+    static async getAllOffers() {
+        try {
+            const res = await axios.get("http://localhost:5000/marketplace");
+            console.log(res);
+            return res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getOfferById(id) {
+        try {
+            const res = await axios.get(`http://localhost:5000/marketplace/${id}`);
+            console.log(res);
+            return res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getMyOffers() {
+        try {
+            const res =  await axios.get("http://localhost:5000/marketplace/myoffers");
+            console.log(res);
+            return res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
